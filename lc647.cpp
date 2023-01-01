3eb4c8e3d3b25373a8aedf3fe23741c2eadd5676
Neel Gupta <neeleshgupta023@gmail.com>
2023-01-01 17:47:56 -0600

    adding 647ish

---
+++ lc647.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool isPal(string s)
+{
+    return true;
+}
+
+int palindromicSubstrings(string s)
+{
+}
+
+int main()
+{
+    return 0;
+}
+
+
